a4aa5c0e0670b73c9f4286f71ca1050f3dfd0cfb
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-03-28 16:38:23 +0530

    gowrist.c

---
+++ gowrist.c
@@ -0,0 +1,11 @@
+def main():
+ num = int(input(""))
+
+ factorial = 1
+
+ if num == 0:
+   print("The factorial of 0 is 1")
+ elif num>0:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print(factorial)
