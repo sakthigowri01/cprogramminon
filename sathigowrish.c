844e1da9906db4d64322182a7b2145e837a0eee2
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-03-28 16:49:16 +0530

    sathigowrish.c

---
+++ sathigowrish.c
@@ -0,0 +1,30 @@
+nclude <stdio.h>
+#include<string.h>
+int main(void) {
+	char a[100],b[100];
+	scanf("%s %s",a,b);
+	int m,n,i,j,u,v,w,x,y,z,flag=0;
+	m=strlen(a);
+	n=strlen(b);
+	if(m==n)
+	{
+	for(i=0;i<m;i++)
+	{
+		for(j=i+1;j<m;j++)
+		{
+			
+	}
+	else
+	{
+		printf("no");
+	}
+	if(flag==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
